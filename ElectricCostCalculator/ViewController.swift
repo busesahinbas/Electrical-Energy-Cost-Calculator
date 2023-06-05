d0f7adb74c2e42329ef56614f1040461f1d5c3cb
busesahinbas <85345494+busesahinbas@users.noreply.github.com>
2023-06-05 15:55:38 +0300

    inital commits

---
+++ ElectricCostCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElectricCostCalculator
+//
+//  Created by Buse Şahinbaş on 5.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
